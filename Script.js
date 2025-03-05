fe933847b09bc8cbd20963cc5842d19610a96117
Kirubel Eshetu <Kirubelwinner@gmail.com>
2025-03-05 13:36:47 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,18 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const portfolioTitle = document.getElementById('portfolio-title');
-    const navigatinglink = document.getElementById('link');
-    if (navigatinglink) {
-        navigatinglink.addEventListener('mouseover', function() {
-            navigatinglink.style.transform = 'translateY(-50px)';
-            navigatinglink.style.transition = 'transform 0.8s';
-        });
-
-        navigatinglink.addEventListener('mouseout', function() {
-            navigatinglink.style.transform = 'translateY(0)';
-        });
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+// Typewriter Effect
+const texts = [
+    "PROGRAMMER",
+    "DEVELOPER",
+    "DESIGNER"
+]
+let speed  =100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charcterIndex = 0;
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed);
     }
-    if (portfolioTitle) {
-        portfolioTitle.textContent = 'Welcome to My Portfolio';
+    else{
+        setTimeout(eraseText, 1000)
     }
-});
-
+}
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+window.onload = typeWriter
