8a3aa35120d7a1201a0524f7fc67d1b6cab5ad14
Kirubel Eshetu <Kirubelwinner@gmail.com>
2025-03-08 09:27:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function hamburg(){
+function hamburg() {
     const navbar = document.querySelector(".dropdown")
     navbar.style.transform = "translateY(0px)"
 }
-function cancel(){
+function cancel() {
     const navbar = document.querySelector(".dropdown")
     navbar.style.transform = "translateY(-500px)"
 }
@@ -12,26 +12,26 @@ const texts = [
     "DESIGNER",
     "PROGRAMMER",
 ]
-let speed  =100;
+let speed = 100;
 const textElements = document.querySelector(".typewriter-text");
 let textIndex = 0;
 let charcterIndex = 0;
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
+function typeWriter() {
+    if (charcterIndex < texts[textIndex].length) {
         textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
         charcterIndex++;
         setTimeout(typeWriter, speed);
     }
-    else{
+    else {
         setTimeout(eraseText, 1000)
     }
 }
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
         setTimeout(eraseText, 50)
     }
-    else{
+    else {
         textIndex = (textIndex + 1) % texts.length;
         charcterIndex = 0;
         setTimeout(typeWriter, 500)
